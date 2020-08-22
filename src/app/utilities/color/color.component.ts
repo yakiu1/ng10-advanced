0785d14f27c485a483a1aa220d51ae3ccd949cf1
yakiu1 <yakiu100@gmail.com>
2020-08-22 14:08:31 +0800

    get router params

--- src/app/utilities/color/color.component.ts
+++ src/app/utilities/color/color.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   templateUrl: './color.component.html',
@@ -8,17 +9,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ColorComponent implements OnInit {
 
   type = 0;
-
+  name = '';
+  tempRe: boolean;
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.type = +this.route.snapshot.paramMap.get('type');
     console.log(this.type);
+    this.name = this.route.snapshot.queryParamMap.get('name');
 
     this.route.paramMap.subscribe(map => {
       console.log('type changing to ' + map.get('type'));
     });
   }
 
+  doNavigate(evt?: MouseEvent) {
+    console.log('clikc!');
+
+    const navigationExtras: NavigationExtras = {
+      // queryParamsHandling: 'preserve',
+      // preserveFragment: true,
+      queryParams: {
+        name: 'Gon',
+        key: 123
+      }
+    }
+
+    this.type++;
+    this.router.navigate(['/utilities/color/' + this.type], navigationExtras);
+  }
 }

ead09e8addf1f043afd5ad563ec37c7f3715436d
yakiu1 <yakiu100@gmail.com>
2020-08-22 11:33:31 +0800

    Add chilren router 'color'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,20 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { ColorComponent } from './utilities/color/color.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
-  { path: '**', component: PathNotFoundComponent }
+  {
+    path: 'utilities',
+    children: [
+      { path: 'color', component: ColorComponent }
+    ]
+  },
+  { path: '**', component: PathNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { ColorComponent } from './utilities/color/color.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { PathNotFoundComponent } from './path-not-found/path-not-found.component
     Page2Component,
     DashboardComponent,
     PathNotFoundComponent,
+    ColorComponent,
   ],
   imports: [
     BrowserModule,

44282ad46d7804ba0439620ceabbe0b522844669
yakiu1 <yakiu100@gmail.com>
2020-08-29 11:06:34 +0800

    :+1:  template form practice
    control properties,
    form status condition , form submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PathNotFoundComponent } from './path-not-found/path-not-found.component
 import { ColorComponent } from './utilities/color/color.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,9 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+
+  data: any = {
+    email: 'gon@miniasp.com',
+    password: '1342134',
+    remember: 'ture'
+  }
 
   constructor(private router: Router, private route: ActivatedRoute) {
 
@@ -15,14 +22,22 @@ export class LoginComponent implements OnInit, OnDestroy {
     document.body.className = 'bg-gradient-primary';
 
 
-    setTimeout(() => {
-      localStorage.setItem('token', '134');
-      console.log('gogogo!');
-      this.router.navigate(['/' + this.route.snapshot.queryParamMap.get('ret')]);
-    }, 3000);
+    // setTimeout(() => {
+    //   localStorage.setItem('token', '134');
+    //   console.log('gogogo!');
+    //   this.router.navigate(['/' + this.route.snapshot.queryParamMap.get('ret')]);
+    // }, 3000);
   }
   ngOnDestroy(): void {
     document.body.className = '';
   }
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+      console.log('submited!');
+    } else {
+      console.log('QQ');
+    }
+  }
 }
